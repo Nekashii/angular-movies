831dfb4d30e508b03da0b55420a2d2023e99cc64
Ricardo C. <nekashii@outlook.com>
2023-12-22 14:59:13 -0600

    finish

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Angular Movies
+Proyecto realizado como prueba técnica de Angular
+### Configuración
+Para poder ejecutar el proyecto es necesario crear un archivo .env en la raíz del proyecto con la siguientes entrada
+```
+API_TOKEN=$TOKEN
+```
+siendo `$TOKEN` un token de acceso para la API de TMDB
+El proyecto debe ejecutarse con el comando `npm run start`
--- src/app/components/movie-table/movie-table.component.ts
+++ src/app/components/movie-table/movie-table.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Movie } from '../../models/movie.model'
-import { DatePipe } from '@angular/common'
+import { CommonModule, DatePipe } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-movie-table',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule, CommonModule],
   templateUrl: './movie-table.component.html',
 })
 export class MovieTableComponent {
@@ -19,9 +20,29 @@ export class MovieTableComponent {
   pageChange = new EventEmitter<number>()
   @Output()
   filterChange = new EventEmitter<string | undefined>()
+  filter: string = ''
+  private readonly _paginationMargin: number = 2
 
-  setPage(page: number): void {
+  get pagesToShow(): number[] {
+    
+    const pagesToShow: number[] = []
+    for (
+      let iterator = this.currentPage - this._paginationMargin, count = 0;
+      count < this._paginationMargin * 2 + 1 && iterator <= this.totalPages;
+      iterator++
+    ) {
+      if (iterator < 1 || iterator > this.totalPages) continue
+      
+      pagesToShow.push(iterator)
+      count++      
+    }
+    return pagesToShow
+  }
+
+  setPage(page: number): void 
+  {
     this.pageChange.emit(page)
+    if (this.filter) this.filter = ''
   }
 
   setFilter(criteria?: string): void {
